de06c9b1872e8054f9daccac59fb1404fefcfe01
nspzoow <nspzoow@foxmail.com>
2023-11-10 22:54:11 +0800

    feat: add andt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { AudioManager } from "./components/AudioManager";
 import Transcript from "./components/Transcript";
 import { useTranscriber } from "./hooks/useTranscriber";
+import { NextUIProvider, Code } from "@nextui-org/react";
+import { Button } from "antd";
 
 function App() {
     const transcriber = useTranscriber();
@@ -11,22 +13,11 @@ function App() {
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
                     Whisper Web
                 </h1>
-                <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    ML-powered speech recognition directly in your browser
-                </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
             </div>
-
-            <div className='absolute bottom-4'>
-                Made with{" "}
-                <a
-                    className='underline'
-                    href='https://github.com/xenova/transformers.js'
-                >
-                    🤗 Transformers.js
-                </a>
-            </div>
+            <Code></Code>
+            <Button color='primary'>Button</Button>
         </div>
     );
 }
